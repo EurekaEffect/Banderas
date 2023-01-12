2700ad36a6b029c6a0199d3dfa2c89f6f7c2d7fc
Eureka <seruiantonenko1337@gmail.com>
2023-01-12 20:10:56 +0200

    [Version: 0.5] URLParser -> FlagParser, docs and improvements

--- src/main/java/dev/eureka/banderas/system/web/FlagParser.java
+++ src/main/java/dev/eureka/banderas/system/web/FlagParser.java
@@ -6,23 +6,27 @@ import lombok.SneakyThrows;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class URLParser {
+public class FlagParser {
     private static final String CODES_URL = "https://countrycode.org";
+    private static final String FLAG_URL = "https://flagcdn.com/256x192/";
 
     private String content;
 
     private final List<Flag> flags = new ArrayList<>();
 
-    public URLParser() {
+    public FlagParser() {
         getContent();
     }
 
+    /**
+     * Parses website for a raw html string.
+     */
     @SneakyThrows
     private void getContent() {
         URLConnection connection =  new URL(CODES_URL).openConnection();
@@ -33,25 +37,42 @@ public class URLParser {
         scanner.close();
     }
 
+    /**
+     * Get and add each flag from the html content
+     */
     public void collectFlags() {
         flags.clear();
 
         List<String> contentLines = List.of(content.split("\\r?\\n"));
 
-        contentLines.forEach((l) -> {
+        for (int i = 0; i < contentLines.size(); i++) {
             Pattern pattern = Pattern.compile("<td>.. / ...</td>");
-            Matcher matcher = pattern.matcher(l);
+            Matcher matcher = pattern.matcher(contentLines.get(i));
 
             if (matcher.find()) {
                 Flag flag = new Flag();
-                flag.parseAndSetCode(l);
+                flag.parseAndSetName(contentLines.get(i - 2));
+                flag.parseAndSetCode(contentLines.get(i));
+                flag.setFlag(String.format("%s%s.png", FLAG_URL, flag.getCode()));
 
                 flags.add(flag);
             }
-        });
+        }
     }
 
+    /**
+     * @return list with flags
+     */
     public List<Flag> getFlags() {
         return flags;
+    }
+
+    /**
+     * @return random flag from the flag list
+     */
+    public Flag getAny() {
+        Random random = new Random();
+
+        return flags.get(random.nextInt(flags.size() - 1));
     }
 }
